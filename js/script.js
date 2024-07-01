e9ab5e77f5054bb737f00f2236e5f679f25cb2b9
Brandon Supinski <66806979+bsupinski@users.noreply.github.com>
2024-07-01 15:30:15 -0400

    added input bar via js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
+const paginationContainer = document.querySelector(".js-page-container");
+const studentListContainer = document.querySelector(".student-list");
 const itemsPerPage = 9;
 
 /*
@@ -15,7 +17,6 @@ Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 function showPage(array, page) {
-  const studentList = document.querySelector(".student-list");
   const start = page * itemsPerPage - itemsPerPage;
   const end = page * itemsPerPage - 1;
   for (let i = 0; i < array.length; i++) {
@@ -50,7 +51,7 @@ function showPage(array, page) {
 
       liContainer.append(studentDetails, joinedDetails);
 
-      studentList.append(liContainer);
+      studentListContainer.append(liContainer);
     }
   }
 }
@@ -60,8 +61,6 @@ Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 function addPagination(array) {
-  const paginationContainer =
-    document.getElementsByClassName("js-page-container")[0];
   const buttonsNeeded = Math.ceil(array.length / itemsPerPage);
   for (let i = 0; i < buttonsNeeded; i++) {
     const listEl = document.createElement("li");
@@ -71,7 +70,47 @@ function addPagination(array) {
     listEl.appendChild(buttonEl);
     paginationContainer.appendChild(listEl);
   }
+
+  const firstButton = document.querySelector("button");
+  firstButton.classList.add("active");
 }
+
+paginationContainer.addEventListener("click", (e) => {
+  closestButton = e.target.closest("button");
+  if (closestButton) {
+    const currentActiveButton = document.querySelector(".active");
+    currentActiveButton.classList.remove("active");
+    closestButton.classList.add("active");
+    studentListContainer.innerHTML = ``;
+    showPage(data, closestButton.innerHTML);
+  }
+});
+
+//Add searchbar
+const header = document.querySelector("header");
+
+const label = document.createElement("label");
+label.setAttribute("for", "search");
+label.classList.add("student-search");
+
+const inputName = document.createElement("span");
+inputName.innerText = "Search by name";
+
+const input = document.createElement("input");
+input.id = "search";
+input.setAttribute("placeholder", "Search by name...");
+
+const inputButton = document.createElement("button");
+inputButton.setAttribute("type", "button");
+
+const buttonImg = document.createElement("img");
+buttonImg.setAttribute("src", "img/icn-search.svg");
+buttonImg.setAttribute("alt", "Search icon");
+inputButton.append(buttonImg);
+
+label.append(inputName, input, inputButton);
+
+header.appendChild(label);
 
 // Call functions
 addPagination(data);
